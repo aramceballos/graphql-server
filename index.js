070818a7302c2b7df1bc9471dcfb629bc6a54b5b
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-03-01 21:48:48 -0700

    GrapgQL initialized

---
+++ index.js
@@ -0,0 +1,11 @@
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+graphql(schema, '{hello}').then((data) => {
+  console.log(data);
+});
