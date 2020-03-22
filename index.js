d8ffd80b7494d44eea08d9c8d6caf654e0f9a8c4
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-03-22 14:17:12 -0700

    Serving the api

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 'use strict';
 
 const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const gqlMiddleware = require('express-graphql');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 const schema = buildSchema(`
     type Query {
-        hello: String,
-        saludo: String
+        hello: String
     }
 `);
 
 const resolvers = {
   hello: () => 'Hello world',
-  saludo: () => 'Hello everybody',
 };
 
-graphql(schema, '{saludo}', resolvers).then((data) => {
-  console.log(data);
+app.use(
+  '/api',
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  }),
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/api`);
 });
