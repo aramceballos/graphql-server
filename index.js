929cb358901637178cb26582a0344bd10d1880bb
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-03-21 23:49:41 -0700

    Using resolvers

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+'use strict';
+
 const { graphql, buildSchema } = require('graphql');
 
 const schema = buildSchema(`
     type Query {
-        hello: String
+        hello: String,
+        saludo: String
     }
 `);
 
-graphql(schema, '{hello}').then((data) => {
+const resolvers = {
+  hello: () => 'Hello world',
+  saludo: () => 'Hello everybody',
+};
+
+graphql(schema, '{saludo}', resolvers).then((data) => {
   console.log(data);
 });
