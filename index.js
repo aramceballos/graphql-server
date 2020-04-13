43e251b57f18138acbfb737f84226d34c0423b53
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-04-12 23:36:42 -0700

    Implementing apollo server and enums

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 require('dotenv').config();
 
-const { makeExecutableSchema } = require('graphql-tools');
-const express = require('express');
-const gqlMiddleware = require('express-graphql');
+const { ApolloServer } = require('apollo-server');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const resolvers = require('./lib/resolvers');
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
 
 const typeDefs = readFileSync(
   join(__dirname, 'lib', 'schema.graphql'),
   'utf-8',
 );
-const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.use(
-  '/api',
-  gqlMiddleware({
-    schema,
-    rootValue: resolvers,
-    graphiql: true,
-  }),
-);
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}/api`);
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server running at ${url}`);
 });
